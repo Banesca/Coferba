af3cacbd3ff051a1ee7cc9e49fcb00ecb3e2a179
jorge Gutierrez <jorguti58@gmail.com>
2017-07-18 21:04:40 -0300

    COMIT STRUCTURA

---
+++ Frond/Modules/user/js/user.js
@@ -0,0 +1,22 @@
+ var app = angular.module('myApp', []);
+      app.controller('myCtrl', function($scope, $http) {
+         
+
+       //  alert();
+         
+
+          $scope.get = function() {
+             
+               $http({
+                  method : "GET",
+                  url : "http://127.0.0.1/baoli/Api/index.php/Client"
+                }).then(function mySuccess(response) {
+                    $scope.listClient = response.data;
+                  }, function myError(response) {
+                   //alert(response.statusText);
+                });
+             
+          }
+
+
+       });
