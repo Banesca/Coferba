9f7c37ecd9f3f56ae64110ee209ed02e3e377ed6
David Rincon <rexx84@gmail.com>
2017-07-23 12:00:50 -0300

    Formularios de nuevos usuarios y login.

--- Frond/Modules/user/js/user.js
+++ Frond/Modules/user/js/user.js
@@ -1,22 +1,37 @@
  var app = angular.module('myApp', []);
-      app.controller('myCtrl', function($scope, $http) {
-         
 
-       //  alert();
-         
+app.controller('myCtrl', function($scope, $http) {
+   
 
-          $scope.get = function() {
-             
-               $http({
-                  method : "GET",
-                  url : "http://127.0.0.1/baoli/Api/index.php/Client"
-                }).then(function mySuccess(response) {
-                    $scope.listClient = response.data;
-                  }, function myError(response) {
-                   //alert(response.statusText);
-                });
-             
-          }
+ // alert();
 
 
-       });
+ $scope.testing = "jorge";
+   
+
+    $scope.get = function() {
+       
+         $http({
+            method : "GET",
+            url : "http://127.0.0.1/baoli/Api/index.php/Client"
+          }).then(function mySuccess(response) {
+              $scope.listClient = response.data;
+            }, function myError(response) {
+             //alert(response.statusText);
+          });
+       
+
+
+    }
+
+
+
+
+    $scope.metodo = function () {
+      // body...
+
+      $scope.testing = $scope.nombre;
+    }
+
+
+ });
