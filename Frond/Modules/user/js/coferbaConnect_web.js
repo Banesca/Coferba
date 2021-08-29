7aae6a98f9547db9e2e613dc69e41bb0b3aa402f
David Rincon <rexx84@gmail.com>
2021-08-29 20:16:35 -0300

    Redistribution of files and folders

--- Frond/Modules/user/js/coferbaConnect_web.js
+++
@@ -1,4 +0,0 @@
-var app = angular.module('coferbaApp', ["blockUI", "inform", "inform-exception", "showdown", "ngAnimate", "ui.bootstrap"]);
-app.controller('coferbaConnect', function($scope) {	
-	$scope.serverHost="http://coferba.com.ar/";
-});
